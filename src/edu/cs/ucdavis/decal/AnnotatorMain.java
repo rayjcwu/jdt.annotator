41488099c1201999a35eb234bb64516eeea752de
Ray Jui-Chung Wu <rayjcwu@gmail.com>
2013-10-03 03:00:02 -0700

    add commons-clii to parse arguments

--- src/edu/cs/ucdavis/decal/AnnotatorMain.java
+++ src/edu/cs/ucdavis/decal/AnnotatorMain.java
@@ -3,30 +3,55 @@ package edu.cs.ucdavis.decal;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+import org.apache.commons.cli.PosixParser;
+
 public class AnnotatorMain {
-	public static void main(String [] argv) {
+	/**
+	 * example argv
+	   --jdbc jdbc:postgresql://127.0.0.1:5432/annotation
+	   --project demo
+	   --src /Users/jcwu/Dropbox/code/project/annotation/fake_project/
+	 */
+	public static void main(String[] argv) {
 		Logger logger = Logger.getLogger("annotator");
 
-		if (argv.length != 1) {
-			logger.log(Level.SEVERE, "need an argument for src/");
-		} else {
-			try {
-				OmniController controller = new OmniController(argv[0]);
-				controller.setLogger(logger);
-				(new PostgreSQLStorer("jdbc:postgresql://127.0.0.1:5432/annotation")).resetDatabase().register(controller);
-				(new DumpAstVisitor()).register(controller);
-				(new AnnotationASTRequestor()).register(controller);
+		Options options = new Options();
+		options.addOption("s", "src", true, "absolute root path of files");
+		options.addOption("p", "project", true, "project name");
+		options.addOption("d", "jdbc", true, "jdbc url, currently only support postgresql (jdbc:postgresql://ip:port/schema)");
+		HelpFormatter formatter = new HelpFormatter();
+		CommandLineParser parser = new PosixParser();
+		CommandLine cmd = null;
 
-				controller.setProjectName("demo2");
+		String jdbc_url = "";
+		String project_name = "";
+		String src_path = "";
+		try {
+			cmd = parser.parse(options, argv);
+			src_path = cmd.getOptionValue("s");
+			project_name = cmd.getOptionValue("p", src_path);
+			jdbc_url = cmd.getOptionValue("d");
+			if (src_path == null || project_name == null || jdbc_url == null) { throw new ParseException(argv.toString()); }
 
-				controller.run();
+			OmniController controller = new OmniController(src_path);
+			controller.setLogger(logger);
+			(new PostgreSQLStorer(jdbc_url)).resetDatabase().register(controller);
+			(new DumpAstVisitor()).register(controller);
+			(new AnnotationASTRequestor()).register(controller);
+			controller.setProjectName(project_name);
+			controller.run();
 
-				System.out.println("finished");
-			} catch (Exception e) {
-				logger.log(Level.SEVERE, e.toString());
-				e.printStackTrace();
-			}
+			System.out.println("finished");
+		} catch (ParseException e) {
+			formatter.printHelp("annotator.jar", options);
+		} catch (Exception e) {
+			logger.log(Level.SEVERE, e.toString());
+			e.printStackTrace();
 		}
-
 	}
 }
