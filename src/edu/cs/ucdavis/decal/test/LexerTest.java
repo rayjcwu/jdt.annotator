71eba71548a3ea203cc5aced8ea1bb88aab0b05f
Ray Jui-Chung Wu <rayjcwu@gmail.com>
2013-10-16 17:00:51 -0700

    try JDT build-it scanner

--- src/edu/cs/ucdavis/decal/test/LexerTest.java
+++ src/edu/cs/ucdavis/decal/test/LexerTest.java
@@ -2,12 +2,57 @@ package edu.cs.ucdavis.decal.test;
 
 import java.lang.reflect.Field;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.antlr.JavaLexer;
+import org.eclipse.jdt.core.compiler.IScanner;
+import org.eclipse.jdt.core.compiler.ITerminalSymbols;
+import org.eclipse.jdt.core.compiler.InvalidInputException;
+import org.eclipse.jdt.internal.core.util.PublicScanner;
 import org.junit.Test;
 
+
+
 public class LexerTest {
+
+	@Test
+	public void testJdtLexer() {
+		IScanner scanner = new PublicScanner();
+		String source = "public class A { int a; List <String> b = new ArrayList<String>(); }";
+		scanner.setSource(source.toCharArray());
+
+		Map <Integer, String> map = new HashMap<Integer, String>();
+		try {
+			for (Field field: ITerminalSymbols.class.getDeclaredFields()) {
+				if (field.getType().equals(int.class)) {
+					int id = field.getInt(null);
+					String string = field.getName().substring(9).toUpperCase();
+					map.put(id, string);
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		final int EOF_ID = 158;
+		int token;
+
+		while (true) {
+			try {
+				token = scanner.getNextToken();
+				if (token == EOF_ID) { break; }
+
+				System.out.println(scanner.getCurrentTokenSource());
+
+				System.out.println(String.format("%s %d -> %d", map.get(token), scanner.getCurrentTokenStartPosition(), scanner.getCurrentTokenEndPosition()));
+			} catch (InvalidInputException e) {
+				break;
+			}
+		}
+
+	}
 
 	@Test
 	public void testLexer() {
